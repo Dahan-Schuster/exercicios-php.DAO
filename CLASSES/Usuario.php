6a74bc9bcc12e41a504ce63e3ec8b4b196e08d57
Dahan Schuster <dan.plschuster@gmail.com>
2019-04-14 10:40:23 -0300

    Classe Usuario

--- CLASSES/Usuario.php
+++ CLASSES/Usuario.php
@@ -4,4 +4,85 @@
 class Usuario
 {
 
+    # Recomenda-se utilizar os mesmos nomes das colunas da tabela no banco
+    # Assim é possível utilizar o fetch style PDO::FETCH_CLASS
+    private $id;
+    private $login;
+    private $senha;
+
+    public function loadById($id)
+    {
+        $sql = new Sql();
+
+        $result = $sql->select("SELECT * FROM tb_usuarios WHERE id = :ID", array(
+            ":ID"=>$id
+        ));
+
+        if (count($result) > 0)
+        {
+            $row = $result[0];
+            $this->setId($row['id']);
+            $this->setLogin($row['login']);
+            $this->setSenha($row['senha']);
+        }
+    }
+
+    public function __toString()
+    {
+        return json_encode(array(
+            "id"=>$this->getId(),
+            "login"=>$this->login,
+            "senha"=>$this->senha
+        ));
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLogin()
+    {
+        return $this->login;
+    }
+
+    /**
+     * @param mixed $login
+     */
+    public function setLogin($login)
+    {
+        $this->login = $login;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSenha()
+    {
+        return $this->senha;
+    }
+
+    /**
+     * @param mixed $senha
+     */
+    public function setSenha($senha)
+    {
+        $this->senha = $senha;
+    }
+
+
 }
