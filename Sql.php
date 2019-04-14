f7713119fb6a7a3d11fb70120ad5cd7f7659ac17
Dahan Schuster <dan.plschuster@gmail.com>
2019-04-14 10:11:26 -0300

    Classe de acesso ao banco de dados

--- Sql.php
+++ Sql.php
@@ -1,7 +1,46 @@
 <?php
 
 
-class Sql
+class Sql extends PDO
 {
+    private $conexao;
 
+    public function __construct()
+    {
+        $user = 'root';
+        $pass = 'root';
+        $this->conexao = new PDO ("mysql:host=localhost;dbname=dbphp7", $user, $pass);
+    }
+
+    private function setParameters($statement, $parameters = array())
+    {
+        foreach ($parameters as $key => $value)
+        {
+            $statement->bindParam($key, $value);
+        }
+    }
+
+    private function setParam($statement, $key, $value)
+    {
+        $statement->bindParam($key, $value);
+    }
+
+    public function query($rawQuery, $params = array())
+    {
+        $statement = $this->conexao->prepare($rawQuery);
+
+        $this->setParameters($statement, $params);
+
+        $statement->execute();
+
+        return $statement;
+
+    }
+
+    public function select($rawQuery, $params = array()):array
+    {
+        $statement = $this->query($rawQuery);
+
+        return $statement->fetchAll(PDO::FETCH_ASSOC);
+    }
 }
